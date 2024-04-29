44ec133d2dd03b9404ca3db52fbe68aac55ef316
KRZYSZTOF_KULKA <krzysztof.kulka1234@gmail.com>
2024-04-29 13:32:48 +0200

    decluttered code

--- decorator.py
+++ decorator.py
@@ -4,7 +4,6 @@ import time
 def log(level):
     def decorator(func):
         if callable(func):
-            # If func is a function
             def wrapper(*args, **kwargs):
                 start = time.time()
                 logging.log(level, f"Calling function {func.__name__} with arguments {args} and {kwargs}")
@@ -14,7 +13,6 @@ def log(level):
                 return result
             return wrapper
         else:
-            # If func is a class
             class Wrapper(func):
                 def __init__(self, *args, **kwargs):
                     logging.log(level, f"Instantiating class {func.__name__} with arguments {args} and {kwargs}")
